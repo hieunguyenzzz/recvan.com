1b806221a9654fe3a0178392d348d82a08c0d0ca
ngohoanglong <ngohoanglongptit@gmail.com>
2022-03-09 18:05:04 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
-import AllComponents from '@components/app/AllComponents'
+import { templateRoutes } from '@utils/componentRoutes'
 
-export default function Home() {
-  return <AllComponents />
+export default function Pages({  ...props }) {
+  const componentRoutes = templateRoutes.find(([key]) => key === 'homepage' )[1]
+  return (
+    <>
+      <input
+        hidden
+        defaultChecked
+        id="data-theme-recvan"
+        data-theme="recvan"
+        name="theme"
+        type="radio"
+        defaultValue="recvan"
+      />
+      <div className="flex flex-col justify-between min-h-screen ">
+        {componentRoutes.map(([key, Component, props]) => {
+          return <Component key={key} {...props} />
+        })}
+      </div>
+    </>
+  )
 }
+
+export async function getStaticProps(context) {
+  const slug = context.params.slug
+  return {
+    props: {
+     
+    },
+  }
+}
+
